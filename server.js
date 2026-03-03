4316902bbffa669c4c29d26f3edb9468925bd9a8
Minikapi <minikapi694@gmail.com>
2026-03-03 18:14:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,13 +10,8 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Отдаём статику
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Если переходят на любую страницу — вернуть index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// Отдаём ВСЮ папку проекта
+app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
